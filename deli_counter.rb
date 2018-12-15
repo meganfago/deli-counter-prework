745bf73e5c0ff3d9983211500dd6161cefa32dfa
Megan Fago <meganlaynefago@gmail.com>
2018-12-15 20:26:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,13 @@
-def deli_counter(line)
-  katz_deli = []
-  
+def line(array) 
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
